16d2f7c096597060ff72d621a2dc8fba1f2d1139
ZiluLiang <candyliang1994@gmail.com>
2023-06-21 19:34:59 +0100

    mid save

---
+++ scripts/secondlevelsearchlight.m
@@ -0,0 +1,7 @@
+searchlight_rsa_dir = 'D:\OneDrive - Nexus365\Project\pirate_fmri\Analysis\data\fmri\unsmoothedLSA\LSA_stimuli_navigation\searchlightrsa';
+
+vselect = {'noselection','reliability_ths0'};
+
+analysis = 
+
+fullfile(searchlight_rsa_dir,vselect)
