e81acb26e0541dcfe1afce5819ed9cbe9ef71ad7
ZiluLiang <candyliang1994@gmail.com>
2023-08-09 14:15:59 +0100

    add rdm based on response location

--- scripts/multivariate/helper.py
+++ scripts/multivariate/helper.py
@@ -151,6 +151,56 @@ def compute_rdm(pattern_matrix:numpy.ndarray,metric:str) -> numpy.ndarray:
     rdm = squareform(pdist(X_drop_na, metric=metric))    
     return rdm
 
+def compute_rdm_identity(identity_arr:numpy.ndarray) -> numpy.ndarray:
+    """calculate model rdm based on stimuli identity, if the pair have the same value, distance will be zero, otherwise will be one.
+
+    Parameters
+    ----------
+    identity_arr: numpy.ndarray
+        a 1D numpy array of size: (nsample,)
+
+    Returns
+    -------
+    numpy.ndarray
+        2D numpy array of model rdm
+    """
+    assert isinstance(identity_arr,numpy.ndarray), "identity_arr must be 1D numpy array"
+    identity_arr = numpy.squeeze(identity_arr)
+    assert identity_arr.ndim == 1, "identity_arr must be 1D numpy array"
+    X,Y = numpy.meshgrid(identity_arr,identity_arr)
+    return 1. - abs(X==Y)# if same, distance=0
+
+def compute_rdm_nomial(pattern_matrix:numpy.ndarray) -> numpy.ndarray:
+    """compute the dissimilarity matrix of a nsample x nfeature matrix where feature values are nomial.
+    Features are assumed to be orthogonal so the distance will be Euclidean distance assuming features are one-hot encoded.
+
+    Parameters
+    ----------
+    pattern_matrix : numpy.ndarray
+        a 2D numpy array of size: nsample x nfeature
+    metric : str
+        dissimilarity/distance metric passed to `scipy.spatial.distance.pdist`
+
+    Returns
+    -------
+    numpy.ndarray
+        a nsample x nsample dissimliarity matrix
+
+    Raises
+    ------
+    Exception
+        pattern matrix must be 2D
+    """
+    assert isinstance(pattern_matrix,numpy.ndarray), "pattern_matrix must be 2D numpy array"
+    assert pattern_matrix.ndim == 2, "pattern_matrix must be 2D numpy array"
+    
+    X = pattern_matrix
+    na_filters = numpy.all([~numpy.isnan(X[j,:]) for j in range(X.shape[0])],0)
+    X_drop_na = X[:,na_filters]
+    feature_rdms = [compute_rdm_identity(X_drop_na[:,k]) for k in range(X_drop_na.shape[1])]
+    rdm = numpy.sqrt(numpy.sum(feature_rdms,axis=0))
+    return rdm
+
 def checkdir(dirs:list or str):
     """check if directories exist, if not, generate directories
 
@@ -213,11 +263,12 @@ class ModelRDM:
     ----------
     stimid : numpy.ndarray
         a 1D numpy array of stimuli ids
-    stimloc : numpy.ndarray
-        a 2D numpy array of stimuli locations (x and y)
+    stimgtloc : numpy.ndarray
+        a 2D numpy array of stimuli groundtruth locations (x and y)
     stimfeature : numpy.ndarray
         a 2D numpy array of stimuli features (color and feature)
-    n_session : int, optional
+    stimresploc : numpy.ndarray
+        a 2D numpy array of stimuli locations (x and y) based on participants response, by default None
         number of sessions, by default 1
     split_sess : bool, optional
         split the rdm into within-session and between-session or not, by default True
@@ -225,37 +276,70 @@ class ModelRDM:
     
     def __init__(self,
                  stimid:numpy.ndarray,
-                 stimloc:numpy.ndarray,
+                 stimgtloc:numpy.ndarray,
                  stimfeature:numpy.ndarray,
                  stimgroup:numpy.ndarray,
+                 stimresploc:numpy.array=None,
                  n_session:int=1,
                  randomseed:int=1,
                  nan_identity:bool=False):
+        
         self.n_session   = n_session
         self.n_stim      = len(stimid)       
         self.stimid      = numpy.tile(stimid,(n_session,1)).reshape((self.n_stim*self.n_session,-1))
-        self.stimloc     = numpy.tile(stimloc,(n_session,1)).reshape((self.n_stim*self.n_session,-1))
+        self.stimloc     = numpy.tile(stimgtloc,(n_session,1)).reshape((self.n_stim*self.n_session,-1)) ## ground-truth location
         self.stimfeature = numpy.tile(stimfeature,(n_session,1)).reshape((self.n_stim*self.n_session,-1))
         self.stimgroup   = numpy.tile(stimgroup,(n_session,1)).reshape((self.n_stim*self.n_session,-1))
         self.stimsession = numpy.concatenate([numpy.repeat(j,len(stimid)) for j in range(self.n_session)])
-
-        models = {"loc2d":self.euclidean2d(),
-                  "cityblock2d":self.cityblock2d(),
-                  "loc1dx":self.euclidean1d(0),
-                  "loc1dy":self.euclidean1d(1),
-                  "feature2d":self.feature2d(),
-                  "feature1dx":self.feature1d(0),
-                  "feature1dy":self.feature1d(1),
-                  "stimuli":self.identity(),
-                  "stimuligroup":self.identity(self.stimgroup),
-                  "shuffledloc2d":self.random(randomseed=randomseed,rdm=self.euclidean2d(),mode="permuterdm"),
-                  "randfeature2d":self.random(randomseed=randomseed,mode="randomfeature"),
-                  "randmatrix":self.random(randomseed=randomseed,mode="randommatrix")
+        if stimresploc is None: ## response location
+            gen_resprdm = False
+        else:
+            gen_resprdm = True
+            if numpy.shape(stimresploc)[0] == self.stimloc.shape[0]:
+                self.stimresploc     = stimresploc
+            else:
+                self.stimresploc     = numpy.tile(stimresploc,(n_session,1)).reshape((self.n_stim*self.n_session,-1))
+        
+        models = {"gtlocEuclidean":   compute_rdm(self.stimloc,metric="euclidean"),
+                  "gtlocCityBlock":   compute_rdm(self.stimloc,metric="cityblock"),
+                  "gtloc1dx":         compute_rdm(self.stimloc[:,[0]],metric="euclidean"),
+                  "gtloc1dy":         compute_rdm(self.stimloc[:,[1]],metric="euclidean"),
+                  "feature2d":        compute_rdm_nomial(self.stimfeature),
+                  "feature1dx":       compute_rdm_identity(self.stimfeature[:,0]),
+                  "feature1dy":       compute_rdm_identity(self.stimfeature[:,1]),
+                  "stimuli":          compute_rdm_identity(self.stimid),
+                  "stimuligroup":     compute_rdm_identity(self.stimgroup)}
+        models |= {
+                  "shuffledloc2d": self.random(randomseed=randomseed,rdm=models["gtlocEuclidean"],mode="permuterdm"),
+                  "randfeature2d": self.random(randomseed=randomseed,mode="randomfeature"),
+                  "randmatrix":    self.random(randomseed=randomseed,mode="randommatrix")
                 }
+        
+        if gen_resprdm:
+            models |= {"resplocEuclidean":  compute_rdm(self.stimresploc,metric="euclidean"),
+                       "resplocCityBlock":  compute_rdm(self.stimresploc,metric="cityblock"),}
+
+        # split into train/test
+        if numpy.unique(self.stimgroup).size>1:
+            U,V = numpy.meshgrid(self.stimgroup,self.stimgroup)
+            WTR = numpy.multiply(1.*(U == 1),1.*(V == 1))
+            WTE = numpy.multiply(1.*(U == 0),1.*(V == 0))
+            WTR[WTR==0]=numpy.nan
+            WTE[WTE==0]=numpy.nan
+            if gen_resprdm:
+                split_models = ['gtlocEuclidean','gtlocCityBlock','feature2d','resplocEuclidean','resplocCityBlock']
+            else:
+                split_models = ['gtlocEuclidean','gtlocCityBlock','feature2d']
+            for k in split_models:
+                wtr_n = 'trainstimpairs_' + k
+                rdmwtr = numpy.multiply(models[k],WTR)
+                wte_n = 'teststimpairs_' + k
+                rdmwte = numpy.multiply(models[k],WTE)
+                models |= {wtr_n:rdmwtr,wte_n:rdmwte}
 
         # split into sessions
         if n_session>1:
-            BS = self.session() # 0 - within session; 1 - within session
+            BS = compute_rdm_identity(self.stimsession) # 0 - within session; 1 - within session
             WS = 1 - BS         # 0 - between session; 1 - between session
             BS[BS==0]=numpy.nan
             WS[WS==0]=numpy.nan
@@ -266,7 +350,7 @@ class ModelRDM:
                 bs_n  = 'between_'+k
                 rdmbs = numpy.multiply(v,BS)
                 models |= {ws_n:rdmws,bs_n:rdmbs}
-            models["session"] = self.session()
+            models["session"] = compute_rdm_identity(self.stimsession)
         for k,v in models.items():
             if nan_identity:
                 from copy import deepcopy
@@ -313,116 +397,8 @@ class ModelRDM:
         elif mode == "randommatrix":
             return prng.random((self.n_stim*self.n_session,self.n_stim*self.n_session))
         else:
-            raise ValueError("invalid random mode, must be one of: randomfeature, permuterdm, randommatrix")
+            raise ValueError("invalid random mode, must be one of: randomfeature, permuterdm, randommatrix")    
 
-    def session(self)->numpy.ndarray:
-        """calculate model rdm based on session, if the pair is in the same session, distance will be 0, otherwise will be 1
-        
-        Returns
-        -------
-        numpy.ndarray
-            2D numpy array of model rdm
-        """
-        S = numpy.zeros((self.n_stim,self.n_stim)) # matrix for same session
-        D = numpy.ones((self.n_stim,self.n_stim)) # matrix for different session
-        
-        r = []
-        for j in range(self.n_session):
-            c = [D for _ in range(self.n_session)]
-            c[j] = S
-            r.append(c)
-        modelrdm = numpy.block(r)
-        return modelrdm
-    
-    def euclidean2d(self)->numpy.ndarray:
-        """calculate model rdm based on 2d euclidean distance.
-        
-        Returns
-        -------
-        numpy.ndarray
-            2D numpy array of model rdm
-        """
-        modelrdm = compute_rdm(self.stimloc,metric="euclidean")
-        return modelrdm
-    
-    def cityblock2d(self)->numpy.ndarray:
-        """calculate model rdm based on 2d euclidean distance.
-        
-        Returns
-        -------
-        numpy.ndarray
-            2D numpy array of model rdm
-        """
-        modelrdm = compute_rdm(self.stimloc,metric="cityblock")
-        return modelrdm
-    
-
-    def euclidean1d(self,dim)->numpy.ndarray:
-        """calculate model rdm based on 1d euclidean distance.
-
-        Parameters
-        ----------
-        dim : int
-        which axis (0:x, 1:y) should be used to calculated rdm.
-
-        Returns
-        -------
-        numpy.ndarray
-            2D numpy array of model rdm
-        """
-        X,Y = numpy.meshgrid(self.stimloc[:,dim],self.stimloc[:,dim])
-        modelrdm = abs(X-Y)
-        return modelrdm
-
-    def feature2d(self)->numpy.ndarray:
-        """calculate model rdm based on both stimuli features, if the pair:
-            shares 0 feature  - sqrt(2);
-            shares 1 feature  - 1;
-            shares 2 features - 0;
-
-        Returns
-        -------
-        numpy.ndarray
-            2D numpy array of model rdm
-        """
-        RDM_attrx = self.feature1d(0)
-        RDM_attry = self.feature1d(1)
-        modelrdm  = numpy.sqrt(RDM_attrx+RDM_attry)
-        return modelrdm
-    
-    def feature1d(self,dim:int)->numpy.ndarray:
-        """calculate model rdm based on 1 stimuli feature, if the pair has the same feature, distance will be zero, otherwise will be one.
-
-        Parameters
-        ----------
-        dim : int
-        which feature should be used to calculated rdm.
-
-        Returns
-        -------
-        numpy.ndarray
-            2D numpy array of model rdm
-        """
-        X,Y = numpy.meshgrid(self.stimfeature[:,dim],self.stimfeature[:,dim])
-        modelrdm = 1. - abs(X==Y) # if same feature, distance=0
-        return modelrdm
-    
-
-    def identity(self,identity_arr=None)->numpy.ndarray:
-        """calculate model rdm based on stimuli identity, if the pair is the same stimuli, distance will be zero, otherwise will be one.
-
-        Returns
-        -------
-        numpy.ndarray
-            2D numpy array of model rdm
-        """
-        if identity_arr is None:
-            X,Y = numpy.meshgrid(self.stimid,self.stimid)
-        else:
-            X,Y = numpy.meshgrid(identity_arr,identity_arr)
-        modelrdm = 1. - abs(X==Y)# if same stimuli, distance=0
-        return modelrdm
-    
     def visualize(self,modelname:str or list="all",tri:int=0,annot:bool=False)->matplotlib.figure:
         """plot model rdms using seaborn heatmap
 
